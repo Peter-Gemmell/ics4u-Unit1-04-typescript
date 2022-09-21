0769c7e9063b241a9c1101f8ec47d27bc35b51be
Peter Gemmell <peter.gemmell@mths.ca~>
2022-09-21 23:18:13 +0000

    x

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ const prompt = promptSync()
 const min = 1
 const max = 6
 const mystery = Math.floor(Math.random() * max) + min
+let guesses = 1
 
 while (true) {
   try {
@@ -21,8 +22,11 @@ while (true) {
     if (isNaN(guess)) {
       throw new Error('not a number')
     }
-    if (guess === mystery) {
+    if (guess < 1 || guess > 6) {
+      console.log('Not an integer between 1-6.')
+    } else if (guess === mystery) {
       console.log('\nYou guessed correctly!')
+      console.log(`It took ${guesses} tries.`)
       break
     } else if (guess > mystery) {
       console.log('That number is too high, guess lower.')
@@ -30,7 +34,8 @@ while (true) {
       console.log('That number is too low, guess higher.')
     }
   } catch (e) {
-    console.log('Not an integer from 1-6.')
+    console.log('Not an integer between 1-6.')
   }
+  guesses = guesses + 1
 }
 console.log('\nDone.')
